aa330225ad03af9acfbc5a89d14c4138cf00834a
kam <devkam.company@gmail.com>
2018-06-16 13:47:33 +0200

    Testing github's gist.

---
+++ 2017/03-azure – srodowisko, narzedzia, ASM i ARM/powershell_sample_ARM.ps1
@@ -0,0 +1,4 @@
+Install-Module AzureRM # instalacja modułu
+Login-AzureRMAccount # logowanie do nowego portalu
+Get-AzureRMSubscription # informacje o subskrypcji
+New-AzureRMResourceGroup # stworzenie nowej grupy zasobów
---
+++ 2017/03-azure – srodowisko, narzedzia, ASM i ARM/powershell_sample_asm.ps1
@@ -0,0 +1,4 @@
+Install-Module Azure # instalacja modułu Azure'owego dla PowerShella
+Get-AzureSubscription # informacje o subskrypcji
+New-AzureStorageContainer # stworzenie magazynu danych dla wirtualnych dysków VHD
+New-AzureService # utworzenie nowego cloud service'u
